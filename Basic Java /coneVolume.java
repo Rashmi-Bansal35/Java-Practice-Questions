fcdd8290b465242c24c609ca5b474f59b2432004
Rashmi-Bansal35 <gupta126idraghav@gmail.com>
2025-07-22 12:12:26 +0530

    Updated

---
+++ Basic Java /coneVolume.java
@@ -0,0 +1,11 @@
+/*
+ Write a program to print the volume of cone.
+ */
+public class coneVolume {
+    public static void main(String[] args){
+        int radius = 4;
+        int height = 9;
+        System.out.println("The volume of cone is: " +(1.0/3 * 3.14 * radius * radius * height));
+    }
+    
+}
---
+++ Basic Java /hemisphereArea.java
@@ -0,0 +1,8 @@
+public class hemisphereArea {
+    public static void main(String[] args){
+        int radius = 8;
+        double result = 2 * 3.14 * radius * radius;
+        System.out.println("The area of hemisphere is " +result);
+    }
+    
+}
