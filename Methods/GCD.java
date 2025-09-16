d0e647e448f56fffab3e0afccf435fb0d0d035e8
Rashmi-Bansal35 <gupta126idraghav@gmail.com>
2025-09-16 18:45:28 +0530

    compute gcd

--- Methods/GCD.java
+++ Methods/GCD.java
@@ -2,10 +2,26 @@ package Methods;
 import java.util.Scanner;
 
 public class GCD {
-    public static int calculateGCD(int firstNumber, int secondNumber){
-        
-        return 0;
+    public static int least(int firstNumber, int secondNumber){
+        if(firstNumber < secondNumber){
+            return firstNumber;
+        }
+        else{
+            return secondNumber;
+        }
     }
+
+    public static int calculateGCD(int firstNumber, int secondNumber){
+        int gcd = 1;
+        int least = least(firstNumber, secondNumber);
+        for(int i = 2; i <= least; i++){
+            if(firstNumber % i == 0 && secondNumber % i == 0){
+                gcd = i;
+            }
+        }
+        return gcd;
+    }
+
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         System.out.print("Enter your first number: ");
