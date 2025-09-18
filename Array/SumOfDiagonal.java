d65e46e02b87dc46fffadec6b7508f6b6f7f6a85
Rashmi-Bansal35 <gupta126idraghav@gmail.com>
2025-09-18 19:37:26 +0530

    sum of diagonal elements

---
+++ Array/SumOfDiagonal.java
@@ -0,0 +1,25 @@
+package Array;
+
+public class SumOfDiagonal {
+    public static int elementSum(int[][] arr){
+        int sum = 0;
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr.length; j++){
+                if(i == j){
+                    sum += arr[i][j];
+                }
+            }
+        }
+        return sum;
+    }
+
+    public static void main(String[] args){
+        int[][] arr = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+        int sum = elementSum(arr);
+        System.out.println("The sum of diagonal elements is: " +sum);
+    }
+}
